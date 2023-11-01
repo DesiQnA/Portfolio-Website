d476000f7d4cdb1dc293974b3edecb05f24a9a01
DesiQnA <149320455+DesiQnA@users.noreply.github.com>
2023-11-01 10:22:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+=======
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+>>>>>>> b969b40870bc6cea9621ffb091c59de2eb076128
+})
